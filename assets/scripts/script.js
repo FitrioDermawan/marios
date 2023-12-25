6e2f73927169ace49f6c1dc7067c5327d189d012
Fitrio-Dermawan <91893718+FitrioDermawan@users.noreply.github.com>
2023-12-25 14:38:39 +0700

    initial-push
    pushing from existing code in local

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+// Slider Script
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 6000);
